99472f067c9605405d67ad158288c79cbbea656d
anelfer <alexone226@yandex.ru>
2023-08-30 00:31:36 +0300

    Initial Commit

---
+++ ictis shedule/ictis_sheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ictis_sheduleApp.swift
+//  ictis shedule
+//
+//  Created by Alexander Rafshnayder on 30.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ictis_sheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
